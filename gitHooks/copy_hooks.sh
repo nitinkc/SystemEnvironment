dd69ec4ef4b90c23475cc3cb8829ef8266821ecd
Nitin Chaurasia <gs.nitin@gmail.com>
2022-09-13 02:20:40 -0500

    copy hooks update

--- gitHooks/copy_hooks.sh
+++ gitHooks/copy_hooks.sh
@@ -8,5 +8,11 @@ DIRS=$(find $DIR -name "*.git")
 for dest in $DIRS; do
   curl -fL -o "$dest/hooks/pre-commit" https://raw.githubusercontent.com/nitinkc/SystemEnvironment/master/gitHooks/pre-commit
   chmod +x "$dest/hooks/pre-commit"
+
+  curl -fL -o "$dest/hooks/pre-push" https://raw.githubusercontent.com/nitinkc/SystemEnvironment/master/gitHooks/pre-push
+  chmod +x "$dest/hooks/pre-push"
+
   stat -f "%A %Sp %N" "$dest/hooks/pre-commit" #works on MacOS
+  stat -f "%A %Sp %N" "$dest/hooks/pre-push" #works on MacOS
+
 done
