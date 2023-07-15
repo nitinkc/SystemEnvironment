86c62d7591051b3064ce9970ededdfb8f4462521
Nitin Chaurasia <gs.nitin@gmail.com>
2023-07-15 00:02:27 -0600

    Update mac_defaults.sh

--- mac/mac_defaults.sh
+++ mac/mac_defaults.sh
@@ -3,7 +3,6 @@
 # Set Sublime as Default Text editor. Find the exact key under CFBundleIdentifier in Info.plist of the App
 #cat /Applications/Development/Sublime\ Text.app/Contents/Info.plist
 #<key>CFBundleIdentifier</key>	<string>com.sublimetext.4</string>
-defaults write com.apple.LaunchServices LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.4;}';killall Finder /System/Library/CoreServices/Finder.app;
 defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.4;}';killall Finder /System/Library/CoreServices/Finder.app;
 
 #Show hidden files
@@ -11,6 +10,10 @@ defaults write com.apple.finder AppleShowAllFiles YES;killall Finder /System/Lib
 
 # Shows the full path on the Finder's Window top
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES;killall Finder
+
+# Disable warning when changing file extensions:
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
 
 # Remove the delays in the Dock
 defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
@@ -34,4 +37,25 @@ defaults write com.apple.screencapture location "$directory"
 ##################### ########## ######################################
 
 
+# Set Finder to display folders on top when sorting by name:
+defaults write com.apple.finder _FXSortFoldersFirst -bool true; killall Finder
+
+
+# Enable spring-loading for folders and windows:
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true; killall Finder
+# Set the spring-loading delay:
+defaults write NSGlobalDomain com.apple.springing.delay -float 0.5; killall Finder
+
+
+# Enable quick look for directories:
+defaults write com.apple.finder QLEnableXRayFolders -bool true; killall Finder
+
+
+# Disable the creation of .DS_Store files on network or USB volumes:
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+
+# Change the default view mode to column view:
+defaults write com.apple.finder FXPreferredViewStyle clmv; killall Finder
 
